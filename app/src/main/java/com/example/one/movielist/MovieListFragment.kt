5b55ca6dfa7b65f316c3fd82760aa09a6e53df58
Sokitbayev <Sokitbayev@gmail.com>
2021-02-04 22:29:06 +0600

    ktx

--- app/src/main/java/com/example/one/movielist/MovieListFragment.kt
+++ app/src/main/java/com/example/one/movielist/MovieListFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -38,10 +39,9 @@ class MovieListFragment : Fragment(), OnItemClickListener {
 
         val dataSource = MovieDatabase.getInstance(application).movieDatabaseDao
 
-        val viewModelFactory = MovieListViewModelFactory(dataSource)
-
-        val movieListViewModel =
-            ViewModelProvider(this, viewModelFactory).get(MovieListViewModel::class.java)
+        val movieListViewModel: MovieListViewModel by viewModels {
+            MovieListViewModelFactory(dataSource)
+        }
         binding.movieListViewModel = movieListViewModel
 
         configureRecyclerView()
@@ -51,9 +51,9 @@ class MovieListFragment : Fragment(), OnItemClickListener {
             binding.refreshLayout.isRefreshing = false
         }
 
-        movieListViewModel.errorMessage.observe(viewLifecycleOwner,{
+        movieListViewModel.errorMessage.observe(viewLifecycleOwner){
             showErrorMessage(it)
-        })
+        }
 
         return binding.root
     }
@@ -61,11 +61,11 @@ class MovieListFragment : Fragment(), OnItemClickListener {
     private fun configureRecyclerView() {
         binding.rvMovie.layoutManager = LinearLayoutManager(context)
         binding.rvMovie.adapter = adapter
-        movieListViewModel.movieList.observe(viewLifecycleOwner, {
+        movieListViewModel.movieList.observe(viewLifecycleOwner) {
             it?.let {
                 adapter.submitList(it)
             }
-        })
+        }
     }
 
     private fun showErrorMessage(e: Exception){
